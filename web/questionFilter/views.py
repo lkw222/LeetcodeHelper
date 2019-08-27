6d75f0175c5bbf8d322790c717ed58aa6c783d24
lkw222 <lionpowder@gmail.com>
2019-08-27 19:08:21 -0400

    add-pagination-basic

--- web/questionFilter/views.py
+++ web/questionFilter/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from questionFilter.models import *
+from django.core.paginator import Paginator
 # Create your views here.
 def home(request):
-    questions = Question.objects.filter(frequency__gte=1)[:50]
+    # questions = Question.objects.filter(frequency__gte=1)[:50]
+    questions = Question.objects.all().order_by('front_id')[:50]
+
     for question in questions:
         question.frequency /= 0.05
-    data = {'questions': questions}
+    limit = 20
+    paginator = Paginator(questions, limit)
+    page = request.GET.get('page')
+    contacts = paginator.get_page(page)
+    data = {'questions': contacts}
     # data = {'question':Question.objects.filter(frequency__gte=1)[:50]}
     return render(request, 'questionFilter/homepage.html', data)
+
+
+# def sorted_by_frequency(request):
+
+
 
 def question_detail(request, slug):
     detail = Question.objects.get(question_slug=slug)
